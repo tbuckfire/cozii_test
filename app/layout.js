ba73a905b9e8136035b0b56dfc709be4136b9f10
TobyWan23 <tqbuckfire@hotmail.com>
2025-02-19 20:59:39 -0500

    Added root layout to fix Next.js App Router

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <head>
+          <title>Mars Visit Application</title>
+        </head>
+        <body>{children}</body>
+      </html>
+    );
+  }
